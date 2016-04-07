3b09281bc1f231fe645f7f09e42813e1f54607cd
Jeremiah Marks <jeremmiah@jlmarks.org>
2016-04-07 16:18:36 -0700

    commiting to ask question

--- ContactsExport.py
+++ ContactsExport.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 # @Author: jeremiah.marks
 # @Date:   2016-04-07 13:51:32
 # @Last Modified by:   Jeremiah Marks
-# @Last Modified time: 2016-04-07 14:41:49
+# @Last Modified time: 2016-04-07 16:18:05
+import logging
 import httplib2
 import time
 import os
@@ -16,8 +16,11 @@ import argparse
 flags = argparse.Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=True)
 SCOPES = 'https://www.googleapis.com/auth/contacts.readonly'
 CLIENT_SECRET_FILE = "C:\\Users\\jeremiah.marks\\Documents\\GitHub\\bexport\\client_secret.json"
-APPLICATION_NAME = 'ContactsToCSV'
-C:\Users\jeremiah.marks\Documents\GitHub\bexport\client_secret.json
+APPLICATION_NAME = 'contactsexportforinfusionsoft'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 def get_credentials():
     """Gets valid user credentials from storage.
 
@@ -49,35 +52,30 @@ def get_credentials():
 def getContacts(nextPageToken=None):
   global service
   if nextPageToken is None:
-    return service.people().connections().list(resourceName='people/me').execute()
+    return service.people().connections().list(resourceName='people/me', pageSize=50).execute()
   else:
-    return service.people().connections().list(resourceName='people/me', pageToken=nextPageToken).execute()
+    return service.people().connections().list(resourceName='people/me', pageSize=50, pageToken=nextPageToken).execute()
 
+allcontacts={}
 def thatMainScript():
   global service
+  global allcontacts
   credentials = get_credentials()
   http = credentials.authorize(httplib2.Http())
   service = discovery.build('people', 'v1', http=http,
       discoveryServiceUrl='https://people.googleapis.com/$discovery/rest')
-  allcontacts={}
+  # allcontacts={}
   nextpagetoken = None
   print "Retrieving all contacts"
   while True:
+    # print "sleeping 10"
+    # time.sleep(10)
     print "total contacts: " + str(len(allcontacts))
     results = getContacts(nextpagetoken)
     connections = results.get('connections', [])
-    print connections
-    for eachcon in connections:
-        time.sleep(0.6)
-        try:
-          k=service.people().get(resourceName=eachcon['resourceName']).execute()
-          print k
-          allcontacts[eachcon['resourceName']] = k
-        except Exception, e:
-            print "did not get "+ eachcon['resourceName']
-            print e
-
+    contacts=service.people().getBatchGet(resourceNames=[ec['resourceName'] for ec in connections]).execute()
+    for eachcon in contacts['responses']:
+        allcontacts[eachcon['requestedResourceName']]=eachcon
     if 'nextPageToken' not in results:
-      break
+      return allcontacts
     nextpagetoken = results['nextPageToken']
-  return allcontacts
