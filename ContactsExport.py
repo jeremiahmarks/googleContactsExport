736e1a2fbd17dfe7de4bf105345d0252c7b8e284
Jeremiah Marks <jeremmiah@jlmarks.org>
2016-04-07 14:44:50 -0700

    quick update

--- ContactsExport.py
+++ ContactsExport.py
@@ -1,12 +1,10 @@
+# -*- coding: utf-8 -*-
 # @Author: jeremiah.marks
-# @Date:   2016-04-06 01:58:26
+# @Date:   2016-04-07 13:51:32
 # @Last Modified by:   Jeremiah Marks
-# @Last Modified time: 2016-04-07 14:41:50
-# @Author: jeremiah.marks
-# @Date:   2016-04-06 01:21:17
-# @Last Modified by:   jeremiah.marks
-# @Last Modified time: 2016-04-06 01:23:21
+# @Last Modified time: 2016-04-07 14:41:49
 import httplib2
+import time
 import os
 
 from apiclient import discovery
@@ -14,10 +12,12 @@ import oauth2client
 from oauth2client import client
 from oauth2client import tools
 import argparse
-flags = argparse.Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=False)
+
+flags = argparse.Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=True)
 SCOPES = 'https://www.googleapis.com/auth/contacts.readonly'
-CLIENT_SECRET_FILE = "C:\\Users\\jeremiah.marks\\Documents\\GitHub\\gdata-python-client\\client_secret.json"
+CLIENT_SECRET_FILE = "C:\\Users\\jeremiah.marks\\Documents\\GitHub\\bexport\\client_secret.json"
 APPLICATION_NAME = 'ContactsToCSV'
+C:\Users\jeremiah.marks\Documents\GitHub\bexport\client_secret.json
 def get_credentials():
     """Gets valid user credentials from storage.
 
@@ -66,13 +66,18 @@ def thatMainScript():
     print "total contacts: " + str(len(allcontacts))
     results = getContacts(nextpagetoken)
     connections = results.get('connections', [])
+    print connections
     for eachcon in connections:
-      allcontacts[eachcon['resourceName']] = dict(eachcon)
+        time.sleep(0.6)
+        try:
+          k=service.people().get(resourceName=eachcon['resourceName']).execute()
+          print k
+          allcontacts[eachcon['resourceName']] = k
+        except Exception, e:
+            print "did not get "+ eachcon['resourceName']
+            print e
+
     if 'nextPageToken' not in results:
       break
     nextpagetoken = results['nextPageToken']
   return allcontacts
-
-
-if __name__ == '__main__':
-    main()
